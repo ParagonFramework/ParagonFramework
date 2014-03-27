8104f1f67ed371c8ba1f710712a5889f0195b016
daniel.dorfmeister <daniel.dorfmeister@mail.fh-hagenberg.at>
2014-03-27 10:13:34 +0100

    Initial version of login page

---
+++ views/scripts/index/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to pimcore!</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="robots" content="noindex, follow" />
+
+    <link rel="stylesheet" href="/pimcore/static/css/login.css" type="text/css" />
+    <script type="text/javascript" src="/pimcore/static/js/lib/jquery.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="/plugins/ParagonFramework/static/css/example.css?_dc=1395907441"/>
+</head>
+<body>
+
+<div id="vcenter" class="">
+    <div id="content">
+        <div id="right">
+            <form id="loginform" method="post" action="#">
+                <label>Username</label>
+                <input id="username" type="text" name="username" />
+
+                <label>Password</label>
+                <input type="password" name="password" />
+
+                <input type="submit" name="submit" value="Login" />
+            </form>
+        </div>
+        <a href="/admin/login/lostpassword" class="lostpassword">Forgot your password</a>
+    </div>
+</div>
+<div id="footer">
+    <a href="https://se45g1ss14.fh-hagenberg.at/">Paragon Framework</a>
+    <br />
+    &copy; 2014 <a href="https://se45g1ss14.fh-hagenberg.at/">Project Group 1</a>, a proud member of the <a href="http://www.elements.at/">FH Hagenberg</a>
+</div>
+
+</body>
+</html>

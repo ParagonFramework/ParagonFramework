5e7038b4fcd5e33b97e360d74de0ca5b52460663
daniel.dorfmeister <daniel.dorfmeister@mail.fh-hagenberg.at>
2014-04-10 10:43:01 +0200

    magic happened  after someone pushed shit to git

---
+++ lib/ParagonFramework/AbstractWrapper.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: S1210307055
+ * Date: 10.04.14
+ * Time: 10:41
+ */
+
+abstract class AbstractWrapper
+{
+    public abstract function getUserByName($name);
+}

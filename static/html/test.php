06672fae82079d3b4a39ee02ffbbef0d5396edc9
andreas.deutinger <andreas.deutinger@students.fh-hagenberg.at>
2014-04-10 08:58:24 +0200

    LayoutPrototype to Logic merge

--- static/html/test.php
+++ static/html/test.php
@@ -1,20 +1,10 @@
 <?php
-	/**
-	 * User: Johannes
-	 * Date: 03.04.14
-	 * Time: 10:28
-	 */
-	include 'common.php';
-	require_once __DIR__ . '/../../models/Product.php';
+/**
+ * User: AD
+ * Date: 04.04.14
+ * Time: 10:28
+ */
+include 'common.php';
 
-	$product  = new Product(1, "Samsung Galaxy Ace", "Mobile Phone", "needs update");
-	$product2 = new Product(2, "Samsung Galaxy Ace", "Mobile Phone", "needs update");
-
-	$products = array(
-		$product, $product2
-	);
-
-	echo $twig->render('index.twig', array(
-		'title'       => "Welcome!",
-		'test'        => "Lorem Ipsum",
-		'products'    => $products));
+echo $twig->render('test.html', array(
+	"title" => "Layout Prototype &middot; Twitter Bootstrap", "roles" => $roles, "menu" => $menu));

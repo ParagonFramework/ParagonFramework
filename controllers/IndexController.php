b89d1d053a503c2a7098a6dc813284cd38e8aebf
michael.pindur <michael.pindur@students.fh-hagenberg.at>
2014-04-11 12:07:20 +0200

    Merge branch 'LayoutPrototype' of se45g1ss14.fh-hagenberg.at:pimcore-org/ParagonFramework into logic

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,10 +1,10 @@
 <?php
 
 
-class ParagonFramework_IndexController extends Pimcore_Controller_Action_Admin1 {
-
-	public function indexAction() {
-
-		// reachable via http://your.domain/plugin/ParagonFramework/index/index
-	}
+class ParagonFramework_IndexController extends Pimcore_Controller_Action_Admin {
+    
+    public function indexAction ()
+    {
+        // reachable via http://your.domain/plugin/ParagonFramework/index/index
+    }
 }

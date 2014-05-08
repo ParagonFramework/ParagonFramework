f0c5310dbb2fe0593e999671824ef3f7cd5c9e67
johannes.zwirchmayr <johannes.zwirchmayr@students.fh-hagenberg.at>
2014-05-08 09:43:30 +0200

    added first implementation of template system

---
+++ views/scripts/templates/footer.php
@@ -0,0 +1,8 @@
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+
+		<?php echo $this->inlineScript()->appendFile('/plugins\ParagonFramework\static\js/bootstrap.min.js'); ?>
+	</body>
+</html>
--- views/scripts/templates/navigationTop.php
+++ views/scripts/templates/navigationTop.php
@@ -1,2 +1,6 @@
-<?php
-
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container">
+		test
+	</div>
+</nav>
+asdf

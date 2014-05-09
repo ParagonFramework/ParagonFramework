9ab3149a1468fd23404cb3128ed9710676448a05
johannes.zwirchmayr <johannes.zwirchmayr@students.fh-hagenberg.at>
2014-05-09 08:36:57 +0200

    changed formatting

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,12 +1,8 @@
 <?php
 
-
 class ParagonFramework_IndexController extends Pimcore_Controller_Action_Admin {
 
 	public function indexAction() {
-
-
-
 		// reachable via http://your.domain/plugin/ParagonFramework/index/index
 
 		$this->view->content = array(

0e0977fdfef8720356192b13240cb7b71ee55dae
johannes.zwirchmayr <johannes.zwirchmayr@students.fh-hagenberg.at>
2014-05-22 11:11:41 +0200

    Revert "Erste Version von pimcore Objekten implementiert."
    This reverts commit def41e91a5e1a6a7995907825ff34f534dc42675.

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -3,53 +3,15 @@
 class ParagonFramework_IndexController extends ParagonFramework_Controller_ActionAdmin {
 
 	public function indexAction() {
-		$this->view->content = array('products' => array());
+		// reachable via http://your.domain/plugin/ParagonFramework/index/index
 
-		$products = new Object_Product_List();
-		$products->load();
-		foreach ($products as $product) {
-			$this->view->content['products'][] = array(
-				"id"	 => $product->product_id,
-				"name"	 => $product->name,
-				"type"	 => $product->product_type,
-				"status" => $product->status
-			);
-		}
-
-		$product = ParagonFramework_IndexController::getProductById(1);
-	}
-
-	/**
-	 * Returns a single product based on a given id.
-	 * 
-	 * @param int $id The id to be looked up.
-	 * @return Pimcore_Object The product fetched from the pimcore database.
-	 */
-	public function getProductById($id) {
-		return Object_Product::getByProduct_Id($id, array('limit' => 1));
-	}
-
-	/**
-	 * Creates a new product based on some necessary parameters, and saves
-	 * the product in the pimcore database.
-	 */
-	public function createProduct() {
-		$id		 = $_POST['product_id'];
-		$name	 = $_POST['name'];
-		$type	 = $_POST['product_type'];
-		$status	 = $_POST['status'];
-
-		$product = Object_Product::create(array(
-					'product_id'	 => $id,
-					'name'			 => $name,
-					'product_type'	 => $type,
-					'status'		 => $status
-		));
-		$product->save();
-	}
-
-	public function test() {
-		
+		$this->view->content = array(
+			title => "Product Overview",
+			products => array(
+				array("id" => 1, "name" => "test", "type" => "cool", "status" => "valid"),
+				array("id" => 2, "name" => "zwirchi", "type" => "eiskoit", "status" => "valid")
+			)
+		);
 	}
 
 }

206f285dd9cd26486544e627ceef6f414d544611
johannes.zwirchmayr <johannes.zwirchmayr@students.fh-hagenberg.at>
2014-05-23 09:25:43 +0200

    Erster draft von dynamischer Statusnachricht. Editbuttons ohne Funktion hinzugefügt.

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,17 +1,63 @@
 <?php
 
-class ParagonFramework_IndexController extends ParagonFramework_Controller_ActionAdmin {
+error_reporting(-1);
+ini_set('error_reporting', E_ALL);
+
+class ParagonFramework_IndexController extends Pimcore_Controller_Action {
 
 	public function indexAction() {
 		// reachable via http://your.domain/plugin/ParagonFramework/index/index
 
-		$this->view->content = array(
-			title => "Product Overview",
-			products => array(
-				array("id" => 1, "name" => "test", "type" => "cool", "status" => "valid"),
-				array("id" => 2, "name" => "zwirchi", "type" => "eiskoit", "status" => "valid")
-			)
-		);
+		$products = new Object_Product_List();
+//		$products = Object_Product::getByStatus("Invalid");
+//		$products = Object_Product::getByCategory("Mobile phone");
+		$products->load();
+
+		foreach ($products as $product) {
+			$missingFields = array();
+
+			if (!isset($product->image)) {
+				$missingFields[] = "Image";
+			}
+
+			$product->status = empty($missingFields) ? "Valid" :
+					implode("/", $missingFields) . " not set";
+			$product->save();
+		}
+
+		$paginator = Zend_Paginator::factory($products);
+		$paginator->setCurrentPageNumber($this->_getParam('page'));
+		$paginator->setItemCountPerPage(10);
+		$this->view->paginator = $paginator;
+	}
+
+	/**
+	 * Returns a single product based on a given id.
+	 * 
+	 * @param int $id The id to be looked up.
+	 * @return Pimcore_Object The product fetched from the pimcore database.
+	 */
+	public function getProductById($id) {
+		return Object_Product::getByProduct_Id($id, array('limit' => 1));
+	}
+
+	/**
+	 * Creates a new product based on some necessary parameters, and saves
+	 * the product in the pimcore database.
+	 */
+	public function createProduct() {
+		$id = $_POST['product_id'];
+		$name = $_POST['name'];
+		$type = $_POST['product_type'];
+		$status = $_POST['status'];
+
+		$product = Object_Product::create(array(
+					'product_id'	 => $id,
+					'name'			 => $name,
+					'product_type'	 => $type,
+					'status'		 => $status
+		));
+		$product->save();
 	}
 
 }
--- views/scripts/templates/products/table.php
+++ views/scripts/templates/products/table.php
@@ -5,16 +5,20 @@
 			<th>Product Name</th>
 			<th>Product Type</th>
 			<th>Status</th>
+			<th>Actions</th>
 		</tr>
 	</thead>
 	<?php
 	foreach ($this->products as $product) {
 		?>
 		<tr>
-			<td><?php echo $product['id'] ?></td>
-			<td><?php echo $product['name'] ?></td>
-			<td><?php echo $product['type'] ?></td>
-			<td><?php echo $product['status'] ?></td>
+			<td><?php echo $product->o_id ?></td>
+			<td><?php echo $product->name ?></td>
+			<td><?php echo $product->category ?></td>
+			<td><?php echo $product->status ?></td>
+			<td>
+				<input type="button" value="Edit"/>
+			</td>
 		</tr>
 		<?php
 	}

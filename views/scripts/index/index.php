1371e2eec934a02b78ecd6095b3931af26292388
stefan.atzlesberger <stefan.atzlesberger@students.fh-hagenberg.at>
2014-05-23 11:16:43 +0200

    Merge branch 'login' of 193.170.192.154:pimcore-org/ParagonFramework into login
    Conflicts:
    	controllers/IndexController.php
    	lib/ParagonFramework/Models/User.php
    	views/scripts/index/index.php

--- views/scripts/index/index.php
+++ views/scripts/index/index.php
@@ -15,18 +15,10 @@ echo $this->partial('templates/navigationTop.php', null);
 	<!-- pagination start -->
 	<?=
 	$this->paginationControl($this->paginator, 'Sliding', 'includes/paging.php', array(
-<<<<<<< HEAD
             'urlprefix'             => '/plugin/ParagonFramework/index/index?page=',
             'appendQueryString'     => true
 	));
 	?>
-=======
-		'urlprefix'			 => '?page=',
-		'appendQueryString'	 => true
-	));
-	?>
-	<!-- pagination end -->
->>>>>>> c5c2f85128e0ddbe4eaefe90d319930e70bad266
 </div>
 <?php
 echo $this->partial('templates/footer.php', null);

3cd74b54269be8632a8b65a309d31849bee35d96
johannes.zwirchmayr <johannes.zwirchmayr@students.fh-hagenberg.at>
2014-10-20 15:34:16 +0200

    Refaktorierte Kontroller - Anpassung auf Object_Abstract.

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,61 +1,52 @@
 <?php
 
-error_reporting(-1);
-ini_set('error_reporting', E_ALL);
+class ParagonFramework_IndexController extends ParagonFramework_Controller_ActionAdmin {
 
-class ParagonFramework_IndexController extends Pimcore_Controller_Action {
-
-    /**
-     * Loads products from pimcore and sets paginator. Evaluate missing fields and sets reason into type.
-     */
     public function indexAction() {
-        $a = Zend_Auth::getInstance();
+
+        $a    = Zend_Auth::getInstance();
         $user = $a->getIdentity();
-        
-        $configReader = ParagonFramework_ConfigReader::getInstance();
-        $condigProduct = $configReader->getProductByGroup($user->getRole());
-        
-        if ($condigProduct == null) {
+
+        $configReader  = ParagonFramework_ConfigReader::getInstance();
+        $configProduct = $configReader->getProductByGroup($user->getRole());
+
+        if ($configProduct == null) {
             throw new Exception("Product is null");
         }
-        
-        $className = $condigProduct->getName() . '_List';
-        
-        $products = new $className();
-        // $products->setCondition("status NOT LIKE ?", "%valid%");
+
+        $className     = $configProduct->getName();
+        $classNameList = $className . '_List';
+
+        $object = new $className();
+        $class  = $object->getClass();
+
+        $products = new $classNameList();
+        $products->setCondition("status NOT LIKE ?", "%valid%");
         $products->load();
 
-
         foreach ($products as $key => $product) {
-                $missingFields = array();
-                $exclude = array('lazyLoadedFields', 'scheduledTasks');
-                $properties = get_object_vars($product);
-                foreach ($properties as $property => $value) {
-                        if (!preg_match("/o_/", $property) &&
-                                        !in_array($property, $exclude) &&
-                                        empty($value)) {
-                                $missingFields[] = $property;
-                        }
+            $missingFields = array();
+            
+            foreach ($class->getFieldDefinitions() as $fieldname => $definition) {
+                //creating getter to object
+                $getterName = "get" . ucfirst($definition->getName());
+                $value = $product->$getterName();
+                if (empty($value)) {
+                    $missingFields[] = $fieldname;
                 }
-                $product->status = implode("/", $missingFields) . " missing";
+            }
+            
+            $product->status = implode("/", $missingFields) . " missing";
         }
 
         $paginator = Zend_Paginator::factory($products);
         $paginator->setCurrentPageNumber($this->_getParam('page'));
         $paginator->setItemCountPerPage(10);
-        $this->view->content = array(
-                'title'		 => 'pimcore.org',
-                'paginator'	 => $paginator
-        );
-    }
 
-    /**
-     * Redirects to edit product view.
-     */
-    public function editAction() {
-        $id = filter_input(INPUT_POST, 'o_id');
-        $product = Object_Product::getById($id);
-        $this->view->product = $product;
+        $this->view->configProduct = $configProduct;
+        $this->view->configReader  = $configReader;
+        $this->view->paginator     = $paginator;
+        $this->view->user          = $user;
     }
 
     /**
@@ -65,7 +56,7 @@ class ParagonFramework_IndexController extends Pimcore_Controller_Action {
      * @return Pimcore_Object The product fetched from the pimcore database.
      */
     public function getProductById($id) {
-            return Object_Product::getByProduct_Id($id, array('limit' => 1));
+        return Object_Abstract::getByProduct_Id($id, array('limit' => 1));
     }
 
     /**
@@ -73,18 +64,29 @@ class ParagonFramework_IndexController extends Pimcore_Controller_Action {
      * the product in the pimcore database.
      */
     public function createProduct() {
-            $id = $_POST['product_id'];
-            $name = $_POST['name'];
-            $type = $_POST['product_type'];
-            $status = $_POST['status'];
+        $id     = $_POST['product_id'];
+        $name   = $_POST['name'];
+        $type   = $_POST['product_type'];
+        $status = $_POST['status'];
 
-            $product = Object_Product::create(array(
-                                    'product_id'	 => $id,
-                                    'name'			 => $name,
-                                    'product_type'	 => $type,
-                                    'status'		 => $status
-            ));
-            $product->save();
+        $product = Object_Abstract::create(array(
+                    'product_id'   => $id,
+                    'name'         => $name,
+                    'product_type' => $type,
+                    'status'       => $status
+        ));
+
+        $product->save();
+    }
+
+    public function editAction() {
+        $a    = Zend_Auth::getInstance();
+        $user = $a->getIdentity();
+
+        $id                  = filter_input(INPUT_POST, 'o_id');
+        $product             = Object_Abstract::getById($id);
+        $this->view->product = $product;
+        $this->view->user    = $user;
     }
 
 }

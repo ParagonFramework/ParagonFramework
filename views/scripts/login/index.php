3d39c937e44d958ef5c6ede274b055908fcf9da3
andreas.deutinger <andreas.deutinger@students.fh-hagenberg.at>
2014-10-23 08:42:23 +0200

    geändert login.css - view switching Dialog hinzugefügt, index.php angepasst

--- views/scripts/login/index.php
+++ views/scripts/login/index.php
@@ -14,12 +14,41 @@
     <link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/plugins/ParagonFramework/static/css/example.css">
     <link rel="stylesheet" type="text/css" href="/plugins/ParagonFramework/static/css/login.css">
+
+    <script type="text/javascript">
+        function toggle_visibility(id) {
+            var e = document.getElementById(id);
+            if(e.style.display == 'block')
+                e.style.display = 'none';
+            else
+                e.style.display = 'block';
+        }
+    </script>
 </head>
 <body>
     <div id="vcenter">
         <div id="content">
             <h2>Login to<br/>Paragon Framework</h2>
-
+            <div id="viewSwitchingDialog" style="display: none;">
+                <div>
+                    <form role="form" id="viewSwitchingForm" method="post" action="/plugin/ParagonFramework/login/login">
+                        <h3>Chose your view</h3>
+                        <br/>
+                        <br/>
+                        <label>View:</label>
+                        <select>
+                            <option value="view1">view1</option>
+                            <option value="view2">view2</option>
+                            <option value="view3">view3</option>
+                            <option value="view4">view4</option>
+                        </select>
+                        <br/>
+                        <br/>
+                        <button type="submit" class="btn btn-default">Next</button>
+                        <input type="button" class="btn btn-default" name="btnCancel" value="Cancel" onclick="toggle_visibility('viewSwitchingDialog');">
+                    </form>
+                </div>
+            </div>
             <div id="login">
                 <form role="form" id="loginform" method="post" action="/plugin/ParagonFramework/login/login">
                     <label for="username">Username</label>
@@ -30,6 +59,7 @@
                     <br/>
 
                     <button type="submit" class="btn btn-default">Login</button>
+                    <input type="button" name="Text 2" value="Text 2 anzeigen" onclick="toggle_visibility('viewSwitchingDialog');">
                     <a style="margin-left: 10px;" href="/admin/login/lostpassword" class="lostpassword">Forgot your password?</a>
                 </form>
             </div>

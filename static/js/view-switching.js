5a875ab081f6ba0d81737ea8e3d2cdcd12d82ddc
stefan.atzlesberger <stefan.atzlesberger@students.fh-hagenberg.at>
2014-10-23 12:08:32 +0200

    geändert site - Relative hinzugefügt geändert view-switching - variable eingeführt

--- static/js/view-switching.js
+++ static/js/view-switching.js
@@ -1,7 +1,10 @@
 function toggle_visibility(id) {
     var e = document.getElementById(id);
-    if(e.style.display == 'block')
-    e.style.display = 'none';
-    else
-    e.style.display = 'block';
+    var estyle = e.style;
+
+    if (estyle.display == 'block') {
+        estyle.display = 'none';
+    } else {
+        estyle.display = 'block';
     }
+}

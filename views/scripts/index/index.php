29b6d6e1bde24967c0ccc4298e3025c4e9c80048
johannes.zwirchmayr <johannes.zwirchmayr@students.fh-hagenberg.at>
2014-11-03 13:47:06 +0100

    viewSwitchingDialog zentriert.

--- views/scripts/index/index.php
+++ views/scripts/index/index.php
@@ -4,19 +4,21 @@
     <!-- View switching dialog -->
     <div id="viewSwitchingDialog" class="viewSwitchingDialog" style="display: none;">
         <div class="test">
-            <form role="form" method="post" action="<?= $this->url(["action" => "changerole" ]) ?>">
+            <form role="form" method="post" action="<?= $this->url(["action" => "changerole"]) ?>">
                 <input type="hidden" name="viewSwitchingDialog_Selected" id="viewSwitchingDialog_Selected"></input>
                 <h3>Choose your view</h3>
                 <br/>
                 <br/>
-                <div class="dropdown">
-                    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
-                        <span name="viewSwitchingDialog_Label" id="viewSwitchingDialog_Label"></span>
-                        <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu" name="viewSwitchingDialog_Dropdown" id="viewSwitchingDialog_Dropdown">
-                    </ul>
-                </div>
+				<div class="viewSwitchingDialog-wrapper">
+					<div class="dropdown">
+						<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
+							<span name="viewSwitchingDialog_Label" id="viewSwitchingDialog_Label"></span>
+							<span class="caret"></span>
+						</button>
+						<ul class="dropdown-menu" role="menu" name="viewSwitchingDialog_Dropdown" id="viewSwitchingDialog_Dropdown">
+						</ul>
+					</div>
+				</div>
                 <br/>
                 <br/>
                 <button type="submit" class="btn btn-default">Next</button>
@@ -28,10 +30,10 @@
 
 	<?= $this->partial('templates/products/table.php', $this); ?>
 	<!-- pagination start -->
-    <?=
+	<?=
 	$this->paginationControl($this->content['paginator'], 'Sliding', 'includes/paging.php', array(
-            'urlprefix'             => $this->url(["action" => "index" ]) . '?page=',
-            'appendQueryString'     => true
+		'urlprefix'			 => $this->url(["action" => "index"]) . '?page=',
+		'appendQueryString'	 => true
 	));
 	?>
 </div>

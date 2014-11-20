e188dca8c2b2609443b1eec973603163a5c4ee9d
stefan.atzlesberger <stefan.atzlesberger@students.fh-hagenberg.at>
2014-11-20 08:17:24 +0100

    geändert navigationTop - Overview zeigt jetzt auf ParagonFramework Index

--- views/scripts/templates/navigationTop.php
+++ views/scripts/templates/navigationTop.php
@@ -35,7 +35,7 @@ if(($user = $this->user)) {
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Overview</a></li>
+                <li class="active"><a href="<?= $this->url(array('controller' => 'index', 'action' => 'index')) ?>">Overview</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Create Product <b class="caret"></b></a>
                     <ul class="dropdown-menu">

5e323b6db05bf425ca814e747b35026806be537e
stefan.atzlesberger <stefan.atzlesberger@students.fh-hagenberg.at>
2014-12-18 08:13:43 +0100

    geändert config - Admin Gruppe geändert hinzugefügt SmartphoneSnipplet

---
+++ static/templates/SmartphoneSnipplet.php
@@ -0,0 +1,12 @@
+<!-- there should be a generic endpoint where the data from the widgets can be sent to -->
+<?php
+$ofh->field("status",$this->product->status);
+$ofh->field("name",$this->product->name);
+$ofh->field("manufacturer",$this->product->manufacturer);
+$ofh->field("features",$this->product->features,["label" => "Awesome Stuff"]);
+$ofh->field("description",$this->product->description,["width" => 300,"height" => 120]);
+$ofh->field("shortText",$this->product->shortText,["width" => 300,"height" => 120]);
+$ofh->field("releaseDate", $this->product->releaseDate);
+$ofh->field("image", $this->product->image);
+$ofh->field("finished", $this->product->finished);
+?>
